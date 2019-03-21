6c6b67cf61022aedbe36ce8a74ce22c4762c0bdd
Raven Hon <hon@codeastar.com>
2019-03-21 17:29:50 +0800

    Update application.py
    use regular expression to compare white listed domain

--- ezw_restful/application.py
+++ ezw_restful/application.py
@@ -2,13 +2,19 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 from ezw_restful_controller import EZWRController
-import os
+import os, re
 
 application = Flask(__name__)
 api = Api(application)
 CORS(application)
 REF_WHITE_LIST = os.environ['REF_WHITE_LIST']
 ref_white_list_arr = [x.strip() for x in REF_WHITE_LIST.split(",")]
+
+def isDomainWhiteList(incoming_url, domain_list): 
+    for domain in domain_list:
+         if incoming_url.find(domain) > 0:
+            return True
+    return False
 
 class Ezw_API(Resource):
 
@@ -20,10 +26,9 @@ class Ezw_API(Resource):
         end_date = json_data['end_date']
         #print(json_data)
 
-        #remote_add = request.remote_addr
-        referrer = request.headers.get("Referer")
+        referrer = re.search("^(http:\/\/|https:\/\/)+[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}", request.headers.get("Referer"))
         #print("Referrer: [{}]".format(referrer))
-        if (referrer in ref_white_list_arr):
+        if (referrer and (isDomainWhiteList(referrer.group(), ref_white_list_arr) > 0)):
             ezw = EZWRController()     
             ezw_reports = ezw.getWeatherReports(start_date, end_date, latitude, 
                                             longitude)
